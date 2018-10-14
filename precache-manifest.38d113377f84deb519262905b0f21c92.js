efe9b4507f6f191478d9823943b187cb4ae2cd09
Petr Krejčík <petr.krejcik@socialbakers.com>
2018-10-14 22:31:50 +0200

    Updates

---
+++ precache-manifest.38d113377f84deb519262905b0f21c92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portal/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9d0029aa1b9d6035a90e",
+    "url": "/portal/static/js/runtime~main.9d0029aa.js"
+  },
+  {
+    "revision": "249c9f85aaaea67c5d6f",
+    "url": "/portal/static/js/main.249c9f85.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/portal/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "249c9f85aaaea67c5d6f",
+    "url": "/portal/static/css/main.7d995281.chunk.css"
+  },
+  {
+    "revision": "91d8ef28e3aebec75f3b2822c7b092ab",
+    "url": "/portal/index.html"
+  }
+];
